644d236accca9730c926107d3c34f7ee6651978f
drako02 <108550930+drako02@users.noreply.github.com>
2024-10-05 23:09:19 +0000

    Update DatabaseConnector.java

--- src/main/java/com/login/loginform/DatabaseConnector.java
+++ src/main/java/com/login/loginform/DatabaseConnector.java
@@ -8,7 +8,7 @@ public class DatabaseConnector {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/admin_logins";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "Strongest2002";
+    private static final String DB_PASSWORD = "******";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
